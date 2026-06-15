2b222bd966b8c31cc05ff65765ab12d8ad177cfc
A2calanche <arturoale1994@gmail.com>
2026-06-15 14:58:36 -0400

    Fixing the css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 import LogIn from "./components/LogIn";
 import SignIn from "./components/SignIn";
 import TodoList from "./components/TodoList";
